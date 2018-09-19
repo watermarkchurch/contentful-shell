cb5340002d172c88bcfd3304c81627d2b221c227
Gordon <gburgett@watermark.org>
2018-09-19 11:40:11 -0500

    Add init function to install all dependencies

--- test/contentful.sh
+++ test/contentful.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bats
 
+log() {
+  >&3 echo "$@" || true
+}
+
+logerr() {
+  >&3 echo -e "${COLOR_RED}$@${COLOR_NC}"
+}
+
+# setup() {
+# }
+
+# teardown() {
+# }
+
 @test "prints help" {
   run bin/contentful -h
-  echo "${lines[1]}"
   [ "$status" -eq 0 ]
   [ "${lines[0]}" =  "bin/contentful <command> [opts]" ]
 }
